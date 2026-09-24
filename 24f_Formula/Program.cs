8fb6e868ac7439d968526d29e1b8dc88330c0a2c
Molnár Attila <molnar.attila@szlgbp.hu>
2026-09-24 08:15:34 +0200

    levelek és hashset

--- 24f_Formula/Program.cs
+++ 24f_Formula/Program.cs
@@ -53,11 +53,58 @@ namespace _24f_Formula
 					return this.muvelet + this.gyerekei[0].ToString();
 
 				if (this.gyerekei.Count == 2) // Ha 2-argumentumú a művelet, akkor közé írjuk és ZÁRÓJELEZÜNK!
-					return $"({this.gyerekei[0]} {this.muvelet} {this.gyerekei[1]})";
+					return $"({this.gyerekei[0].ToString()} {this.muvelet} {this.gyerekei[1].ToString()})";
 				
 				// Ha több-argumentumú a művelet, akkor leírjuk a műveleti jelet, zárójelet nyitunk, és felsoroljuk az argumentumokat vesszővel elválasztva
 				return $"{this.muvelet}({string.Join(", ", this.gyerekei)})";
 			}
+
+			public int Negaciok_szama()
+			{
+				if (this.gyerekei.Count == 0)
+					return 0;
+
+				int db = 0;
+				foreach (Formula gyerek in this.gyerekei)
+					db += gyerek.Negaciok_szama();
+
+				return this.muvelet == "-" ? 1 + db : db;
+			}
+			public int Levelek_száma() // Hány különböző levélre bomlik a fa?
+			{
+				if (this.gyerekei.Count == 0)
+					return 1;
+
+				int db = 0;
+				foreach (Formula gyerek in gyerekei)
+				{
+					db += gyerek.Levelek_száma();
+				}
+				return db;
+			}
+
+			//public int Levelek_száma() => this.gyerekei.Count == 0 ? 1 : gyerekei.Sum(x => x.Levelek_száma());
+
+			public HashSet<Formula> Atomjai() // Hány különböző "betűből" épül fel! (ne legyen duplázódás)
+			{
+
+			}
+
+			public int Mélység() // hány emelet magas a fa
+			{
+
+			}
+
+
+			public HashSet<Formula> Részformulái() // A fában található összes részformula halmaza
+			{
+
+			}
+
+
+
+
+
 		}
 
 
@@ -65,10 +112,16 @@ namespace _24f_Formula
 
 		static void Main(string[] args)
 		{
+
+
+
+
 			Formula A = new Formula("A");
 			Formula B = new Formula("B");
 			Formula C = new Formula("C");
 			Formula D = new Formula("D");
+
+			Formula aesb = A * B;
 
 			// (A ∧ B ) →¬(C ∨ D)
 
@@ -77,7 +130,7 @@ namespace _24f_Formula
 
 			Console.WriteLine(f);
 
-
+			Console.WriteLine(f.Negaciok_szama());
 
 		}
 	}
